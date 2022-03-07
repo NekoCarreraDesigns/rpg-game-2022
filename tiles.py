8b53455c9f8f69c9408aae2f4b848c69f32e35dc
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-07 12:01:22 -0700

    fixed path error, and init typo

--- tiles.py
+++ tiles.py
@@ -4,7 +4,7 @@ from settings import *
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
-        super().__init(groups)
+        super().__init__(groups)
         self.image = pygame.image.load(
-            './graphics/test/rock.png').convert_alpha()
+            './level graphics/graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
