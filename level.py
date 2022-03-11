960e85f66f537c521ef7f32da378a2e126ec46f8
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-11 11:02:41 -0700

    added ground display to world

--- level.py
+++ level.py
@@ -49,11 +49,22 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        self.floor_surface = pygame.image.load(
+            './level graphics/graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft=(0, 0))
+
 # draw method for the camera class
 
     def custom_draw(self, player):
+        # getting the offset for the player
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface, floor_offset_pos)
+
+        # sprites
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
