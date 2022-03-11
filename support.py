1ad24cae72c11b2c739657d093e3ce1acea0b69c
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-11 12:00:12 -0700

    added support.py with csv reader import and function to read csv files for game

---
+++ support.py
@@ -0,0 +1,13 @@
+# import for csv reader
+from csv import reader
+
+# function to read csv files for game
+
+
+def import_csv_layout(path):
+    terrain_map = []
+    with open(path) as level_map:
+        layout = reader(level_map, delimiter=',')
+        for row in layout:
+            terrain_map.append(list(row))
+        return terrain_map
