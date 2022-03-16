f6e75de8658d465b10df9c34d964516632f7e32b
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-16 14:19:46 -0600

    added input for weapon change

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ from support import import_folder
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, obstacle_sprites, create_attack):
+    def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack):
         super().__init__(groups)
         self.image = pygame.image.load(
             './level graphics/graphics/test/player.png').convert_alpha()
@@ -32,8 +32,12 @@ class Player(pygame.sprite.Sprite):
 
         # weapons
         self.create_attack = create_attack
+        self.destroy_attack = destroy_attack
         self.weapon_index = 0
         self.weapon = list(weapon_data.keys())[self.weapon_index]
+        self.can_switch_weapon = True
+        self.weapon_switch_time = None
+        self.switch_duration_cooldown = 200
 
 
 # player animations
@@ -101,6 +105,15 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
                 print('magic')
+            # weapon change
+            if keys[pygame.K_q] and self.can_switch_weapon:
+                self.can_switch_weapon = False
+                self.weapon_switch_time = pygame.time.get_ticks()
+                if self.weapon_index < len(list(weapon_data.keys())) - 1:
+                    self.weapon_index += 1
+                else:
+                    self.weapon_index = 0
+                self.weapon = list(weapon_data.keys())[self.weapon_index]
 
 
 # method for movement direction on the x and y axis, with speed correction for diagonal movement
@@ -134,14 +147,17 @@ class Player(pygame.sprite.Sprite):
                         self.hitbox.bottom = sprite.hitbox.top
                     if self.direction.y < 0:
                         self.hitbox.top = sprite.hitbox.bottom
-# cooldown method for attackingand magic
+# cooldown method for attacking and magic
 
     def cooldowns(self):
         current_time = pygame.time.get_ticks()
         if self.attacking:
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.attacking = False
-
+                self.destroy_attack()
+        if not self.can_switch_weapon:
+            if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
+                self.can_switch_weapon = True
 # method for player animations for idle, attack, and movement
 
     def animate(self):
