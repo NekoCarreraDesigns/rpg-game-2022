01630a6cfb6ac4f0348dc2fbb63f9f9c0468eb8f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-17 12:51:24 -0600

    added ui colors and general colors, ui size variable to settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,26 @@ fps = 60
 screen_width = 1200
 screen_height = 720
 
+# ui
+bar_height = 20
+health_bar_width = 200
+Energy_bar_width = 140
+item_box_size = 80
+ui_font = './level graphics/graphics/font/joystix.ttf'
+ui_font_size = 18
+
+# ui colors
+health_color = 'red'
+energy_color = 'blue'
+ui_border_color_active = 'gold'
+
+# general colors
+water_color = '#71ddee'
+ui_bg_color = '#222222'
+ui_border_color = '#111111'
+text_color = '#EEEEEE'
+
+# weapons
 weapon_data = {
     'sword': {'cooldown': 100, 'damage': 15, 'graphic': './level graphics/graphics/weapons/sword/full.png'},
     'lance': {'cooldown': 400, 'damage': 30, 'graphic': './level graphics/graphics/weapons/lance/full.png'},
