cfdd6b50d84547e72e774171f09c7e866d84ea6f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-18 11:15:39 -0600

    added Enemy inheritance, and removed collision, and movement functions from file and put them in enemy.py

--- player.py
+++ player.py
@@ -2,11 +2,12 @@
 import pygame
 from settings import *
 from support import import_folder
+from enemy import Enemy
 
 # Player class to display the player sprite
 
 
-class Player(pygame.sprite.Sprite):
+class Player(Enemy):
     def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack, create_magic):
         super().__init__(groups)
         self.image = pygame.image.load(
@@ -144,38 +145,6 @@ class Player(pygame.sprite.Sprite):
                     self.magic_index = 0
                 self.magic = list(magic_data.keys())[self.magic_index]
 
-
-# method for movement direction on the x and y axis, with speed correction for diagonal movement
-
-
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-
-        self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
-        self.rect.center = self.hitbox.center
-
-# collision detection for vertical and horizontal collisions within level with static sprites
-
-    def collision(self, direction):
-        if direction == 'horizontal':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0:
-                        self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x < 0:
-                        self.hitbox.left = sprite.hitbox.right
-
-        if direction == 'vertical':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0:
-                        self.hitbox.bottom = sprite.hitbox.top
-                    if self.direction.y < 0:
-                        self.hitbox.top = sprite.hitbox.bottom
 # cooldown method for attacking and magic
 
     def cooldowns(self):
