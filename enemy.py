888809e8834275752d0e999afe0d8581fa198b0d
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-18 11:14:15 -0600

    created enemy class with movement, aand collision functions

---
+++ enemy.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+
+    # method for movement direction on the x and y axis, with speed correction for diagonal movement
+
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.hitbox.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.hitbox.y += self.direction.y * speed
+        self.collision('vertical')
+        self.rect.center = self.hitbox.center
+
+# collision detection for vertical and horizontal collisions within level with static sprites
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0:
+                        self.hitbox.right = sprite.hitbox.left
+                    if self.direction.x < 0:
+                        self.hitbox.left = sprite.hitbox.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0:
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0:
+                        self.hitbox.top = sprite.hitbox.bottom
