aac51734be820f2002133bc29cf91ae0c229a834
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-18 11:51:16 -0600

    created monster.py with monster class

---
+++ monster.py
@@ -0,0 +1,13 @@
+import pygame
+from settings import *
+from enemy import Enemy
+
+
+class Monster(Enemy):
+    def __init__(self, monster_name, pos, groups):
+        super().__init(groups)
+        self.sprite_type = 'enemy'
+
+        # graphics for enemies
+        self.image = pygame.Surface((64, 64))
+        self.rect = self.image.get_rect(topleft=pos)
