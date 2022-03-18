ed0f436060e8801e19c1c71f05c6faa479e48e69
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-18 12:07:46 -0600

    fixed init error

--- monster.py
+++ monster.py
@@ -5,7 +5,7 @@ from enemy import Enemy
 
 class Monster(Enemy):
     def __init__(self, monster_name, pos, groups):
-        super().__init(groups)
+        super().__init__(groups)
         self.sprite_type = 'enemy'
 
         # graphics for enemies
