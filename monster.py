dc7593cda331e99b80c84a52a7cc8591cf046c43
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-22 12:23:55 -0600

    added damage methods, hit cooldown, and check_death methods

--- monster.py
+++ monster.py
@@ -36,6 +36,11 @@ class Monster(Enemy):
         self.attack_time = None
         self.attack_cooldown = 400
 
+        # hit timer
+        self.vulnerable = True
+        self.hit_time = None
+        self.hit_duration = 300
+
     def import_graphics(self, name):
         self.animations = {'idle': [], 'move': [], 'attack': []}
         main_path = f'./level graphics/graphics/monsters/{name}/'
@@ -86,16 +91,40 @@ class Monster(Enemy):
         self.image = animation[int(self.frame_index)]
         self.rect = self.image.get_rect(center=self.hitbox.center)
 
-    def cooldown(self):
+    def cooldowns(self):
+        current_time = pygame.time.get_ticks()
         if not self.can_attack:
-            current_time = pygame.time.get_ticks()
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.can_attack = True
 
+        if not self.vulnerable:
+            if current_time - self.hit_time >= self.hit_duration:
+                self.vulnerable = True
+
+    def get_damage(self, player, attack_type):
+        if self.vulnerable:
+            self.direction = self.get_player_distance_direction(player)[1]
+            if attack_type == 'weapon':
+                self.health -= player.get_full_weapon_damage()
+            else:
+                pass
+            self.hit_time = pygame.time.get_ticks()
+            self.vulnerable = False
+
+    def check_death(self):
+        if self.health <= 0:
+            self.kill()
+
+    def hit_reaction(self):
+        if not self.vulnerable:
+            self.direction *= -self.resistance
+
     def update(self):
+        self.hit_reaction()
         self.move(self.speed)
         self.animate()
-        self.cooldown()
+        self.cooldowns()
+        self.check_death()
 
     def enemy_update(self, player):
         self.get_status(player)
