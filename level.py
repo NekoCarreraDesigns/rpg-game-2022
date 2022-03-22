935b155d30b17d894307e0a3ddfa443d2ca39521
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-22 12:58:25 -0600

    added damge_player method

--- level.py
+++ level.py
@@ -89,7 +89,7 @@ class Level:
                                         (x, y),
                                         [self.visible_sprites,
                                             self.attackable_sprites],
-                                        self.obstacle_sprites)
+                                        self.obstacle_sprites, self.damage_player)
 
 # method for adding the weapon animations
 
@@ -125,6 +125,12 @@ class Level:
                         else:
                             target_sprite.get_damage(
                                 self.player, attack_sprite.sprite_type)
+
+    def damage_player(self, amount, attack_type):
+        if self.player.vulnerable:
+            self.player.health -= amount
+            self.player.vulnerable = True
+            self.player.hurt_time = pygame.time.get_ticks()
 
     def run(self):
         self.visible_sprites.custom_draw(self.player)
