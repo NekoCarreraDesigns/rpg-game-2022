1b6db5e376aad2debbaa25aa1fdd57d4704492d1
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-25 10:36:21 -0600

    added upgrade cost dictionary

--- player.py
+++ player.py
@@ -50,6 +50,8 @@ class Player(Enemy):
                       'attack': 10, 'magic': 4, 'speed': 6}
         self.max_stats = {'health': 300, 'energy': 140,
                           'attack': 20, 'magic': 10, 'speed': 12}
+        self.upgrade_cost = {'health': 100, 'energy': 100,
+                             'attack': 100, 'magic': 100, 'speed': 100}
         self.health = self.stats['health'] * 0.5
         self.energy = self.stats['energy'] * 0.8
         self.exp = 420
