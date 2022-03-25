bc34565160a0f46c627303e9c411a67e9383ed7a
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-25 10:53:38 -0600

    added exp to check_death method

--- monster.py
+++ monster.py
@@ -5,7 +5,7 @@ from support import *
 
 
 class Monster(Enemy):
-    def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player, trigger_death_particles):
+    def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player, trigger_death_particles, add_exp):
         super().__init__(groups)
         self.sprite_type = 'enemy'
 
@@ -37,6 +37,7 @@ class Monster(Enemy):
         self.attack_cooldown = 400
         self.damage_player = damage_player
         self.trigger_death_particles = trigger_death_particles
+        self.add_exp = add_exp
 
         # hit timer
         self.vulnerable = True
@@ -123,6 +124,7 @@ class Monster(Enemy):
         if self.health <= 0:
             self.kill()
             self.trigger_death_particles(self.rect.center, self.monster_name)
+            self.add_exp(self.exp)
 
     def hit_reaction(self):
         if not self.vulnerable:
