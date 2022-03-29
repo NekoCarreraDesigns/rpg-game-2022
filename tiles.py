58176c3e7fa1b5d886d47a7c7c749793d627fa2d
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-29 11:07:48 -0600

    added offset to tiles

--- tiles.py
+++ tiles.py
@@ -9,10 +9,11 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((tile_size, tile_size))):
         super().__init__(groups)
         self.sprite_type = sprite_type
+        y_offset = hitbox_offset[sprite_type]
         self.image = surface
         if sprite_type == 'object':
             self.rect = self.image.get_rect(
                 topleft=(pos[0], pos[1] - tile_size))
         else:
             self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(0, -10)
+        self.hitbox = self.rect.inflate(0, y_offset)
