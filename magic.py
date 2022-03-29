7621024c9f897204ade436b3ddd22f08bec06b0b
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-29 11:51:55 -0600

    added sounds for heal, and flame spells

--- magic.py
+++ magic.py
@@ -7,9 +7,14 @@ from random import randint
 class MagicPlayer:
     def __init__(self, animation_player):
         self.animation_player = animation_player
+        self.sounds = {
+            'heal': pygame.mixer.Sound('./level graphics/audio/heal.wav'),
+            'flame': pygame.mixer.Sound('./level graphics/audio/Fire.wav')
+        }
 
     def heal(self, player, strength, cost, groups):
         if player.energy >= cost:
+            self.sounds['heal'].play()
             player.health += strength
             player.energy -= cost
             if player.health >= player.stats['health']:
@@ -22,7 +27,7 @@ class MagicPlayer:
     def flame(self, player, cost, groups):
         if player.energy >= cost:
             player.energy -= cost
-
+            self.sounds['flame'].play()
             if player.status.split('_')[0] == 'right':
                 direction = pygame.math.Vector2(1, 0)
             elif player.status.split('_')[0] == 'left':
